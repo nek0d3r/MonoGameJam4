1dd727fb1e18c4cb9ceda42019d5dc5febd1383f
Sebastian Perry <darklink.perry@gmail.com>
2022-01-09 14:33:22 -0500

    added scripts for easy dev env install

---
+++ Easy Install/RunLatest.bat
@@ -0,0 +1,73 @@
+@echo off
+
+: Environment variables
+set @netmin=3.1&                                        : Minimum .NET SDK version
+set @gitfile=Git-2.34.1-64-bit&                         : Filename of git installer
+set @project=MonoGameJam4\&                             : Project directory
+set @gitrepo=https://github.com/nek0d3r/MonoGameJam4&   : Git repository URL
+
+@REM .NET DEPENDENCY
+
+echo Checking for .NET...
+: If dotnet CLI command succeeds
+dotnet --version >nul 2>&1 && (
+    : Output version to temp file
+    dotnet --version>temp
+    : Read contents of temp file
+    for /f %%i in (temp) do (
+        : If the version listed is greater than the minimum
+        if %%i geq %@netmin% (
+            echo .NET SDK %%i found!
+        ) else (
+            : Install .NET SDK LTS using Microsoft powershell script
+            echo .NET SDK version %%i is too old. Installing LTS...
+            Powershell.exe -File ./dotnet-install.ps1
+        )
+    )
+    : Remove temp file
+    del temp
+) || (
+    : Install .NET SDK LTS using Microsoft powershell script
+    echo .NET SDK not found. Installing latest LTS...
+    Powershell.exe -File ./dotnet-install.ps1
+)
+
+@REM GIT DEPENDENCY
+
+echo,
+echo Checking for Git...
+: If git CLI command succeeds
+git --version >nul 2>&1 && (
+    echo Git found!
+) || (
+    : Install git for windows using provided installer
+    : and script written by Pat Migliaccio
+    echo Git not found. Installing Git...
+    call install-git.bat %@gitfile%
+)
+
+@REM PROJECT DEPENDENCY
+
+echo,
+echo Checking if repository is cloned...
+: Project is cloned if directory exists
+if exist %@project% (
+    echo Project source code is already cloned!
+) else (
+    : Clone project using given URL
+    echo Project not cloned. Cloning...
+    git clone %@gitrepo%
+)
+
+: Pull latest changes
+echo,
+echo Pulling changes from latest commit...
+git -C %@project% pull
+
+: Build and run
+echo,
+echo Source code up to date! Compiling and running...
+dotnet build %@project%
+dotnet run --project %@project%
+
+pause
