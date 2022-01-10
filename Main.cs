55c6e10e1684bad11e7fb4f845d92588a7265358
Sebastian Perry <darklink.perry@gmail.com>
2022-01-09 22:03:37 -0500

    fixed buffer calculation on window handler, added texture rendering for debugging

--- Main.cs
+++ Main.cs
@@ -7,14 +7,23 @@ namespace MonoGameJam4
 {
     public static class Globals
     {
-        // Default size of buffer
-        public static readonly Point DEFAULT_WINDOW_SIZE = new Point(800, 800);
+        // Default size of window
+        public static readonly Point DEFAULT_WINDOW_SIZE = new Point(800, 400);
 
-        // Current size of buffer
+        // Current size of window
         public static Point currentWindowSize = DEFAULT_WINDOW_SIZE;
 
-        // How to apply render to buffer
-        public static Rectangle renderDims = new Rectangle(Point.Zero, DEFAULT_WINDOW_SIZE);
+        // Pixel depth
+        public static readonly int PIXEL_DEPTH = 16;
+
+        // Buffer tile dimensions
+        public static readonly Point BUFFER_TILE_DIMS = new Point(40, 20);
+
+        // Buffer size
+        public static readonly Point BUFFER_SIZE = new Point(BUFFER_TILE_DIMS.X * PIXEL_DEPTH, BUFFER_TILE_DIMS.Y * PIXEL_DEPTH);
+
+        // How to apply buffer to render target
+        public static Rectangle renderDims;
     }
 
     public class Main : Game
@@ -22,6 +31,8 @@ namespace MonoGameJam4
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
         private RenderTarget2D _render;
+
+        private Texture2D pixel;
         
         // Captured window size before fullscreen
         private Point previousWindowSize;
@@ -50,16 +61,38 @@ namespace MonoGameJam4
             // Update buffer bounds
             Globals.currentWindowSize.X = Window.ClientBounds.Width;
             Globals.currentWindowSize.Y = Window.ClientBounds.Height;
+            
+            var windowAspectRatio = (float)Globals.currentWindowSize.X / Globals.currentWindowSize.Y;
+            var bufferAspectRatio = (float)Globals.BUFFER_SIZE.X / Globals.BUFFER_SIZE.Y;
 
-            // If width is greater, fill to height
-            if (Globals.currentWindowSize.X > Globals.currentWindowSize.Y)
-                Globals.renderDims = new Rectangle(Globals.currentWindowSize.X / 2 - Globals.currentWindowSize.Y / 2, 0, Globals.currentWindowSize.Y, Globals.currentWindowSize.Y);
-            // If height is greater, fill to width
-            else if (Globals.currentWindowSize.Y > Globals.currentWindowSize.X)
-                Globals.renderDims = new Rectangle(0, Globals.currentWindowSize.Y / 2 - Globals.currentWindowSize.X / 2, Globals.currentWindowSize.X, Globals.currentWindowSize.X);
-            // Buffer is square, fill to bounds
+            Point origin, dimensions;
+
+            // If buffer aspect ratio is higher than window aspect ratio, fill to width
+            if (bufferAspectRatio > windowAspectRatio)
+            {
+                dimensions.X = Globals.currentWindowSize.X;
+                dimensions.Y = (int)(Globals.currentWindowSize.Y / ((float)bufferAspectRatio / windowAspectRatio));
+                origin.X = 0;
+                origin.Y = Globals.currentWindowSize.Y / 2 - dimensions.Y / 2;
+            }
+            // If window aspect ratio is higher than buffer aspect ratio, fill to height
+            else if (windowAspectRatio > bufferAspectRatio)
+            {
+                dimensions.X = (int)(Globals.currentWindowSize.X / ((float)windowAspectRatio / bufferAspectRatio));
+                dimensions.Y = Globals.currentWindowSize.Y;
+                origin.X = Globals.currentWindowSize.X / 2 - dimensions.X / 2;
+                origin.Y = 0;
+            }
+            // Window aspect ratio matches buffer, fill to bounds
             else
-                Globals.renderDims = new Rectangle(0, 0, Globals.currentWindowSize.X, Globals.currentWindowSize.Y);
+            {
+                dimensions.X = Globals.currentWindowSize.X;
+                dimensions.Y = Globals.currentWindowSize.Y;
+                origin.X = 0;
+                origin.Y = 0;
+            }
+
+            Globals.renderDims = new Rectangle(origin, dimensions);
         }
 
         protected override void Initialize()
@@ -72,12 +105,16 @@ namespace MonoGameJam4
             _graphics.PreferredBackBufferWidth = Globals.DEFAULT_WINDOW_SIZE.X;
             _graphics.PreferredBackBufferHeight = Globals.DEFAULT_WINDOW_SIZE.Y;
             _graphics.ApplyChanges();
+
+            WindowChanged(null, null);
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-            _render = new RenderTarget2D(GraphicsDevice, Globals.currentWindowSize.X, Globals.currentWindowSize.Y);
+            _render = new RenderTarget2D(GraphicsDevice, Globals.BUFFER_SIZE.X, Globals.BUFFER_SIZE.Y);
+
+            pixel = Content.Load<Texture2D>("pixel");
         }
 
         protected override void Update(GameTime gameTime)
@@ -127,11 +164,23 @@ namespace MonoGameJam4
             GraphicsDevice.Clear(Color.Black);
 
             // TODO: Draw here
+            _spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend, SamplerState.PointClamp);
+            for(var i = 0; i < Globals.BUFFER_TILE_DIMS.Y; i++)
+            {
+                for(var j = 0; j < Globals.BUFFER_TILE_DIMS.X; j++)
+                {
+                    if((j % 2 == 0 && i % 2 == 0) || (j % 2 != 0 && i % 2 != 0))
+                    {
+                        _spriteBatch.Draw(pixel, new Rectangle(j * Globals.PIXEL_DEPTH, i * Globals.PIXEL_DEPTH, Globals.PIXEL_DEPTH, Globals.PIXEL_DEPTH), null, Color.Blue * 1f);
+                    }
+                }
+            }
+            _spriteBatch.End();
 
             GraphicsDevice.SetRenderTarget(null);
             GraphicsDevice.Clear(Color.White);
 
-            _spriteBatch.Begin(); // If using sprites: SpriteSortMode.Immediate, BlendState.AlphaBlend, SamplerState.PointClamp
+            _spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend, SamplerState.PointClamp);
             _spriteBatch.Draw((Texture2D)_render, Globals.renderDims, Color.White);
             _spriteBatch.End();
 
